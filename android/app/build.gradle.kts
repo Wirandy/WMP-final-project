9f0f1c41f16074ff18fb589a00406e2cf9047e15
BaeYirene <angelinayolandachristinlubis@gmail.com>
2025-11-27 16:55:03 +0700

    design and profile

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,12 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // Firebase services
+    id("dev.flutter.flutter-gradle-plugin") // Flutter plugin
 }
 
 android {
-    namespace = "com.example.moneymanager.money_manager"
+    namespace = "com.wmpfinalproject"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,14 +16,12 @@ android {
     }
 
     kotlinOptions {
+
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.moneymanager.money_manager"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -35,13 +30,12 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
-    source = "../.."
+    source = "../.." //ini path flutter yg dihubungin
 }
+
--- android/app/src/main/kotlin/com/example/moneymanager/money_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/moneymanager/money_manager/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.moneymanager.money_manager
+package com.wmpfinalproject
 
 import io.flutter.embedding.android.FlutterActivity
 
